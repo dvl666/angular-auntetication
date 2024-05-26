50e7c678a9eee4798b5e861bc86a0cb219a275a8
dvl666 <kamagaspar@gmail.com>
2024-05-26 17:39:25 -0400

    primero commit

---
+++ angular-login-frontend/src/app/login/login.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    FormsModule
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+  constructor(
+    private router: Router,
+  ) {}
+
+  username: string = ''
+  password: string = ''
+
+}
